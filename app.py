f5802e354cfc4251d6ca8f79771663f4f148b784
Tetiana Mohorian <133663565+tetianamohorian@users.noreply.github.com>
2025-04-22 01:46:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,6 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
